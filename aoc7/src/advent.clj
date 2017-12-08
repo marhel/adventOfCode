8529b1279f69a4fff632c157229a0062c3ce96b8
Martin Hellspong <martin.hellspong@factor10.com>
2017-12-08 17:44:11 +0100

    Simplify Clojure

--- aoc7/src/advent.clj
+++ aoc7/src/advent.clj
@@ -1,13 +1,13 @@
 (ns advent
-  (:use clojure.string)
-  )
+  (:use clojure.string))
+
 
 (def code "pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)\n")
 
 (defn weight [tree node] (let [imm (get-in tree [node :weight])
                                dep (get-in tree [node :deps])
-                               dsu (apply + (map #(weight tree %) dep))
-                               ]
+                               dsu (apply + (map #(weight tree %) dep))]
+
                            (+ imm dsu)))
 (defn partition-2
   "Partitions the collection into exactly two [[all-truthy] [all-falsy]]
@@ -44,13 +44,13 @@
         atree (reduce add-accum wtree keys)
         unbalanced (map #(nth % 1)
                         (filter (fn [node] (let [das (:dep-acc (nth node 1))]
-                                        (if (empty? das) false (not (apply = das))))) atree))
+                                            (if (empty? das) false (not (apply = das))))) atree))
         lowest (apply min-key :accum unbalanced)
         suspects (group-by (fn [[key weight]] weight)
                            (zipmap (:deps lowest) (:dep-acc lowest)))
         [[[badweight [[badkey]]]] [[goodweight]]] (partition-2 #(= 1 (count (nth % 1))) suspects)
-        badkeyweight (:weight (badkey atree))
-        ]
-    [badkey (- badkeyweight (- badweight goodweight))]
-    ))
+        badkeyweight (:weight (badkey atree))]
+
+    [badkey (- badkeyweight (- badweight goodweight))]))
+
 
--- aoc7/src/advent8.clj
+++ aoc7/src/advent8.clj
@@ -1,6 +1,6 @@
 (ns advent8
-  (:use clojure.string)
-  )
+  (:use clojure.string))
+
 
 (def code "b inc 5 if a > 1
 a inc 1 if b < 5
@@ -9,26 +9,25 @@ c inc -20 if c == 10
 ")
 (def code (slurp "../aoc8/input8.1.txt"))
 
+(def ops {"inc" + "dec" -})
+(def preds {">"  >
+            "<"  <
+            ">=" >=
+            "<=" <=
+            "!=" not=
+            "==" =})
+
+(defn evaluate [env [dst op change src pred lim]]
+  (let [val (get env dst 0)
+        src (get env src 0)
+        change (Integer/parseInt change)
+        lim (Integer/parseInt lim)]
+    (if ((get preds pred =) src lim)
+      (assoc env dst ((get ops op +) val change))
+      env)))
+
 (defn interpreter [code]
-  (let [lines (split-lines code)
-        ops {"inc" +
-             "dec" -}
-        preds {">"  >
-               "<"  <
-               ">=" >=
-               "<=" <=
-               "!=" not=
-               "==" =}
-        evaluate (fn [env [dst op change src pred lim]]
-                   (let [_ (print env [dst op change src pred lim])
-                         val (get env dst 0)
-                         src (get env src 0)
-                         change (Integer/parseInt change)
-                         lim (Integer/parseInt lim)]
-                     (if ((get preds pred =) src lim)
-                       (assoc env dst ((get ops op +) val change))
-                       env)))
-        ]
+  (let [lines (split-lines code)]
     (->> lines
          (map (comp rest first (partial re-seq #"(\w+) (inc|dec) (-?\d+) if (\w+) (<|>|<=|>=|==|!=) (-?\d+)")))
          (reduce evaluate {})
@@ -38,23 +37,6 @@ c inc -20 if c == 10
 
 (defn interpreter-max [code]
   (let [lines (split-lines code)
-        ops {"inc" +
-             "dec" -}
-        preds {">"  >
-               "<"  <
-               ">=" >=
-               "<=" <=
-               "!=" not=
-               "==" =}
-        evaluate (fn [env [dst op change src pred lim]]
-                   (let [;_ (print env [dst op change src pred lim])
-                         val (get env dst 0)
-                         src (get env src 0)
-                         change (Integer/parseInt change)
-                         lim (Integer/parseInt lim)]
-                     (if ((get preds pred =) src lim)
-                       (assoc env dst ((get ops op +) val change))
-                       env)))
         all-envs (->> lines
                       (map (comp rest first (partial re-seq #"(\w+) (inc|dec) (-?\d+) if (\w+) (<|>|<=|>=|==|!=) (-?\d+)")))
                       (reductions evaluate {})
@@ -62,10 +44,9 @@ c inc -20 if c == 10
         env-max (fn [env] (apply max-key #(nth % 1) env))
         maxes (->> (map env-max all-envs)
                    (map (comp first rest)))]
-    [(last maxes) (apply max maxes)]
-    ))
+    [(last maxes) (apply max maxes)]))
 
-(repeatedly 100 #(time (interpreter-max code)))
+(repeatedly 3 #(time (interpreter code)))
 
 
 
