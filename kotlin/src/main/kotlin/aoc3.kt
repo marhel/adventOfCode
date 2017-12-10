3bf01c356b4dca20a3889e5004ff063cc36295f3
Martin Hellspong <martin.hellspong@factor10.com>
2017-12-10 17:11:54 +0100

    AoC 3.2 [Kotlin]

--- kotlin/src/main/kotlin/aoc3.kt
+++ kotlin/src/main/kotlin/aoc3.kt
@@ -1,19 +1,89 @@
 package adventOfCode
 
+data class Point(val col: Int, val row: Int) {
+    fun neighbors(): List<Point> {
+        val ns = mutableListOf<Point>()
+        for (colOffset in (-1..1)) {
+            (-1..1)
+                    .filter { colOffset != 0 || it != 0 }
+                    .mapTo(ns) { Point(col + colOffset, row + it) }
+        }
+        return ns
+    }
+}
+
+/*
+    -4  -3  -2  -1   0.  1.  2   3   4
+4   65  64  63  62  61  60  59  58  57
+3   66  37  36  35  34  33  32  31  56
+2   67  38  17  16  15  14  13  30  55
+1   68  39  18   5   4   3  12  29  54
+0   69  40  19   6   1   2! 11  28  53
+-1  70  41  20   7   8   9  10! 27  52
+-2  71  42  21  22  23  24  25  26! 51
+-3  72  43  44  45  46  47  48  49  50!
+-4  73  74  75  76  77  78  79  80  81  82!
+
+*/
 data class Spiral(val num: Int) {
     val side: Int = Math.ceil(Math.sqrt(num.toDouble())).toInt() / 2 * 2 + 1
     val offset: Int = (side - 1) / 2 - 1
     val start: Int = (side - 2) * (side - 2) + 1
-    val horiz = Math.abs(((num - start) % (side - 1)) - offset)
+    val index: Int = num - start
+    val horiz = if (side < 2) 0 else Math.abs(((num - start) % (side - 1)) - offset)
     val distance: Int = offset + 1 + horiz
-
+    val col: Int = when {
+        num < 2 -> 0
+        index < side - 1 -> {
+            print("#c1"); offset + 1
+        }
+        index < (side - 1) * 2 -> {
+            print("#c2"); (side - 1) * 2 - index - side / 2 - 1
+        }
+        index < (side - 1) * 3 -> {
+            print("#c3"); -(offset + 1)
+        }
+        index <= side * side -> {
+            print("#c4"); index - (side - 1) * 3 - side / 2 + 1
+        }
+        else -> -9999
+    }
+    val row: Int = when {
+        num < 2 -> 0
+        index < side - 1 -> {
+            print("#r1"); index - side / 2 + 1
+        }
+        index < (side - 1) * 2 -> {
+            print("#r2"); offset + 1
+        }
+        index < (side - 1) * 3 -> {
+            print("#r3"); (side - 1) * 3 - index - side / 2 - 1
+        }
+        index <= side * side -> {
+            print("#r4"); -(offset + 1)
+        }
+        else -> -9999
+    }
+    val coord: Point = Point(col, row)
     override fun toString(): String {
-        return String.format("%d: %d %d %d %d-%d = %d\n", num, side, offset, horiz, start, end, distance)
+        return String.format("%d: %d %d @%s\n", num, side, index, coord)
     }
 
     val end: Int = side * side
 }
 
-fun g() = (2..50).map { i -> Spiral(i) }
+fun aoc32() {
+    val grid = mutableMapOf(Pair(Point(0, 0), 1))
+    var lastScore = 0
+    var index = 2
+    while (lastScore < 312051) {
+        val spiral = Spiral(index++)
+        lastScore = spiral.coord.neighbors().fold(0, { acc, point -> grid.getOrDefault(point, 0) })
+        grid.put(spiral.coord, lastScore)
+    }
+    return lastScore
+}
 
 fun spiral(max: Int): Pair<Int, Int> = Pair(max, Spiral(max).distance)
+}
+
---
+++ kotlin/src/test/kotlin/aoc3tests.kt
@@ -0,0 +1,98 @@
+package adventOfCode
+
+import io.kotlintest.forAll
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.properties.forAll
+import io.kotlintest.properties.headers
+import io.kotlintest.properties.row
+import io.kotlintest.properties.table
+import io.kotlintest.specs.StringSpec
+
+val answers = listOf(1, 1, 2, 4, 5, 10, 11, 23, 25, 26, 54, 57, 59, 122, 133, 142, 147, 304, 330, 351, 362, 747, 806)
+
+class SpiralTests : StringSpec() {
+    var banks2hot = arrayOf(14, 0, 15, 12, 11, 11, 3, 5, 1, 6, 8, 4, 9, 1, 8, 4)
+
+    val coordinates = table(
+            headers("pos", "col", "row"),
+            row(1, 0, 0),
+            row(2, 1, 0),
+            row(3, 1, 1),
+            row(4, 0, 1),
+            row(5, -1, 1),
+            row(6, -1, 0),
+            row(7, -1, -1),
+            row(8, 0, -1),
+            row(9, 1, -1),
+            row(10, 2, -1),
+            row(11, 2, 0),
+            row(12, 2, 1),
+            row(13, 2, 2),
+            row(14, 1, 2),
+            row(15, 0, 2),
+            row(16, -1, 2),
+            row(17, -2, 2),
+
+            row(18, -2, 1),
+            row(19, -2, 0),
+            row(20, -2, -1),
+            row(21, -2, -2),
+
+            row(22, -1, -2),
+            row(23, 0, -2),
+            row(24, 1, -2),
+            row(25, 2, -2),
+
+            row(28, 3, 0),
+            row(34, 0, 3),
+            row(35, -1, 3),
+            row(36, -2, 3),
+            row(39, -3, 1),
+            row(47, 1, -3),
+            row(50, 4, -3),
+            row(64, -3, 4),
+            row(65, -4, 4),
+            row(66, -4, 3),
+
+            row(76, -1, -4),
+            row(82, 5, -4)
+    )
+
+    init {
+        "aoc 3.1 distance" {
+            val myTable = table(
+                    headers("pos", "distance"),
+                    row(1, 0),
+                    row(12, 3),
+                    row(23, 2),
+                    row(1024, 31),
+                    row(312051, 430)
+            )
+            forAll(myTable, { pos, distance ->
+                Spiral(pos).distance shouldBe(distance)
+            })
+        }
+        "aoc 3.1 pos 12" {
+            Spiral(12).distance shouldBe(3)
+        }
+        "aoc 3.1 pos 23" {
+            Spiral(23).distance shouldBe(2)
+        }
+        "aoc 3.1 pos 1024" {
+            Spiral(1024).distance shouldBe(31)
+        }
+        "aoc 3.1 pos 1" {
+            Spiral(312051).distance shouldBe(430)
+        }
+        forAll(coordinates, { pos, col, row ->
+            "aoc 3.2 coord pos $pos should have col $col" {
+                Spiral(pos).coord.col shouldBe(col)
+            }
+        })
+        forAll(coordinates, { pos, col, row ->
+        "aoc 3.2 coord pos $pos should have row $row" {
+                Spiral(pos).coord.row shouldBe(row)
+            }
+        })
+    }
+}
