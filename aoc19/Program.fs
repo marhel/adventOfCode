1c37d3e9e5f30a4acbb3bae2d95b1af3fecf93f7
Martin Hellspong <martin.hellspong@factor10.com>
2018-12-01 09:18:37 +0100

    Day 19

---
+++ aoc19/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let grid = argv 
+                |> List.ofSeq
+                |> List.head
+                |> File.ReadAllLines
+
+    printfn "!%s!" grid.[0]
+    grid
+    |> List.ofSeq
+    |> List.map (fun line -> printfn "!%d!" (String.length line))
+    |> ignore
+    0 // return an integer exit code
