bc959bd1c1aa30015498c48971a8a7b81c70d329
Martin Hellspong <martin.hellspong@factor10.com>
2018-12-01 09:18:49 +0100

    Day 20

---
+++ aoc20/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main argv =
+    let get c line = 
+        let m = Regex.Match(line, c + "=<(-?\\d+?),(-?\\d+?),(-?\\d+?)>")
+        (int(m.Groups.[1].ToString()), int(m.Groups.[2].ToString()), int(m.Groups.[3].ToString()))
+    let dist (x, y, z) = abs(x) + abs(y) + abs(z)
+    let grid = argv 
+               |> List.ofSeq
+               |> List.head
+               |> File.ReadAllLines
+               |> List.ofSeq
+               |> Seq.map (get "a" >> dist)
+               |> Seq.mapi (fun i d -> (i, d))
+               |> Seq.minBy snd
+               |> (fun (i, d) -> printfn "%d %d" i d)
+             //|> Seq.iter (fun t -> printfn "%d %d %d = %d" <||| t <| (dist <||| t))
+    0 // return an integer exit code
