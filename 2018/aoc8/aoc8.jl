7e71943738fa4fe4f97f2d735b407be06312f81f
Martin Hellspong <martin.hellspong@factor10.com>
2018-12-12 15:20:36 +0100

    AoC 8.2 [Julia]

--- 2018/aoc8/aoc8.jl
+++ 2018/aoc8/aoc8.jl
@@ -26,3 +26,42 @@ function node(arr, i)
     metasum += sum(view(arr, (1:metadata) .+ pos))
     pos + metadata, metasum
 end
+
+
+struct Node
+    start
+    stop
+    meta
+    children
+end
+value(n) = if length(n.children) == 0
+    println("value of $(n.start) w/o children")
+    sum(n.meta)
+else
+    println("value of $(n.start) w/children")
+    sum(Int[value(n.children[m]) for m in n.meta if m in 1:length(n.children)])
+end
+
+function parseNodes(arr, i)
+    childcount = arr[i]
+    metacount = arr[i+1]
+    println("node $childcount/$metacount at ", i)
+    pos = i + 1
+    metasum = 0
+    children = Node[]
+    for c in 1:childcount
+        child = parseNodes(arr, pos+1)
+        push!(children, child)
+        pos = child.stop
+    end
+    meta = view(arr, (1:metacount) .+ pos)
+    Node(i, pos + metacount, meta, children)
+end
+
+function part2(line)
+    numstrs = split(line, ' ')
+    numbers = map(v -> parse(Int, v), numstrs)
+    root = parseNodes(numbers, 1)
+    println(root)
+    println(value(root))
+end
